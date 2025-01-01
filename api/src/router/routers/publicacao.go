99ae326e5f5cdcdbd509a10b086669869301724e
ppscarvalho <ppscarvalho@gmail.com>
2025-01-01 13:20:16 -0300

    Criando rotas de publicacao

---
+++ api/src/router/routers/publicacao.go
@@ -0,0 +1,40 @@
+package routers
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+// Cria rotas de Publicacao
+var rotaspublicacoes = []Rota{
+	{
+		Uri:                "/publicacoes",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.CriarPublicacao,
+		RequerAutenticacao: true,
+	},
+	{
+		Uri:                "/publicacoes",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarPublicacoes,
+		RequerAutenticacao: true,
+	},
+	{
+		Uri:                "/publicacoes/{id}",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarPublicacao,
+		RequerAutenticacao: true,
+	},
+	{
+		Uri:                "/publicacoes",
+		Metodo:             http.MethodPut,
+		Funcao:             controllers.AtualizarPublicacao,
+		RequerAutenticacao: true,
+	},
+	{
+		Uri:                "/publicacoes/{id}",
+		Metodo:             http.MethodDelete,
+		Funcao:             controllers.DeletarPublicacao,
+		RequerAutenticacao: true,
+	},
+}

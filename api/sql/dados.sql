a081c430b331f729a937be309034759b8f0ac4fd
ppscarvalho <ppscarvalho@gmail.com>
2025-01-02 18:09:53 -0300

    Ajuste nos arquivos sql

--- api/sql/dados.sql
+++ api/sql/dados.sql
@@ -5,6 +5,8 @@ VALUES
 ('Gael Diego Pietro Jesus', 'gael', 'gael@gmail.com', '$2a$10$Sx28.w1nkgXbETmryrH0FOMyN6poQZrgMhjWcFlbHkM121/6rUkEK'),
 ('Murilo Carlos Enzo Duarte', 'murilo', 'murilo.carlos@gmail.com', '$2a$10$Sx28.w1nkgXbETmryrH0FOMyN6poQZrgMhjWcFlbHkM121/6rUkEK'),
 ('Oliver Lucca Lopes', 'oliver', 'oliver@gmail.com', '$2a$10$Sx28.w1nkgXbETmryrH0FOMyN6poQZrgMhjWcFlbHkM121/6rUkEK');
+('João da Silva', 'joao', 'joao@gmail.com', '$2a$10$Sx28.w1nkgXbETmryrH0FOMyN6poQZrgMhjWcFlbHkM121/6rUkEK');
+('Augusto Ferreira Nunes', 'augusto', 'agusto@gmail.com', '$2a$10$Sx28.w1nkgXbETmryrH0FOMyN6poQZrgMhjWcFlbHkM121/6rUkEK');
 
 INSERT INTO seguidores(IdUsuario, IdSeguidor) 
 VALUES
